3835a638251649dadc8beb0eb77e924b97960ef1
Timofii0507 <timofiiyurkov@gamil.com>
2023-11-10 21:35:39 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+int  main()
+{
+	//SetConsoleCP(1251);
+	//SetConsoleOutputCP(1251);
+	cout << "\t\t\t\t\t\tH & I • The Beautiful & Damned" << endl;
+	cout << "" << endl;
+	cout << "\t\t\t\t\t\tThe Beautiful & Damned" << endl;
+	cout << "\t\t\t\t\t\tCross my heart, hope to die" << endl;
+	cout << "\t\t\t\t\t\tTo my lover, I'd never lie" << endl;
+	cout << "\t\t\t\t\t\tHe said be true, I swear I'll try" << endl;
+	cout << "\t\t\t\t\t\tIn the end, it's him and I" << endl;
+	cout << "\t\t\t\t\t\tHe's out his head, I'm out my mind" << endl;
+	cout << "\t\t\t\t\t\tWe got that love, the crazy kind" << endl;
+	cout << "\t\t\t\t\t\tI am his, and he is mine" << endl;
+	cout << "\t\t\t\t\t\tIn the end, it's him and I, him and I" << endl;
+}
