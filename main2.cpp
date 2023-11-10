d0c3042b78ebb0e7ac1564d4231f293d487663f5
Timofii0507 <timofiiyurkov@gamil.com>
2023-11-10 21:56:51 +0200

    Друге завдання з  дз

---
+++ main2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main2()
+{
+	cout << "Every" << "\n";
+	cout << "\tHunter" << "\n";
+	cout << "\t\tWants" << "\n";
+	cout << "\t\t\tTo know" << "\n";
+	cout << "\t\t\t\t Where" << "\n";
+	cout << "\t\t\t\t\tDoes Pheasant" << "\n";
+	cout << "\t\t\t\t\t\t\tSit" << "\n\n";
+}
