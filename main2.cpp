7174c2349a07dae0016dbb3cc8cacec0416ac01a
Timofii0507 <timofiiyurkov@gamil.com>
2023-11-12 20:20:52 +0200

    Друге  завдання  з  дз

--- main2.cpp
+++ main2.cpp
@@ -2,13 +2,22 @@
 #include <Windows.h>
 using namespace std;
 
-int main2()
+int main()
 {
-	cout << "Every" << "\n";
-	cout << "\tHunter" << "\n";
-	cout << "\t\tWants" << "\n";
-	cout << "\t\t\tTo know" << "\n";
-	cout << "\t\t\t\t Where" << "\n";
-	cout << "\t\t\t\t\tDoes Pheasant" << "\n";
-	cout << "\t\t\t\t\t\t\tSit" << "\n\n";
+	HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(output, 12);
+	cout << "Every" << endl;
+	SetConsoleTextAttribute(output, 6);
+	cout << "\tHunter" << endl;
+	SetConsoleTextAttribute(output, 14);
+	cout << "\t\tWants" << endl;
+	SetConsoleTextAttribute(output, 10);
+	cout << "\t\t\tTo know" << endl;
+	SetConsoleTextAttribute(output, 9);
+	cout << "\t\t\t\t Where" << endl;
+	SetConsoleTextAttribute(output, 11);
+	cout << "\t\t\t\t\tDoes Pheasant" << endl;
+	SetConsoleTextAttribute(output, 5);
+	cout << "\t\t\t\t\t\t\tSit" << endl;
+	return 0;
 }
